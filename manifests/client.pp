74a4d8d5401493d11d58aff7caaf14c426093028
Monty Taylor <mordred@inaugust.com>
2015-06-28 12:28:29 -0400

    Add support for CentOS6
    Let's discuss the myriad of ways in which this is broken, but let's do
    it later while crying into our beer. For now, this installs GPG public
    keys and yum repo files, then attempts to install the OpenAFS packages
    as per the instructions here:
    
      http://wiki.openafs.org/RpmClientInstallationWithDKMS/
    
    Depends-On: I663ce7dfcb9663ce24e63c50308df5e51841731c
    Change-Id: Id745d9be78da338f4985f5a8ab185caeefb1e71b

--- manifests/client.pp
+++ manifests/client.pp
@@ -23,32 +23,96 @@ class openafs::client (
     ensure  => present,
   }
 
-  file { '/etc/openafs/afs.conf.client':
+  if ($::osfamily == 'RedHat') {
+    $openafs_path = '/usr/vice/etc'
+
+    $dkms_packages = [
+      'kernel-devel',
+      'dkms',
+      'gcc'
+    ]
+
+    package { $dkms_packages:
+      ensure => present,
+      before  => [
+        Package['openafs-client'],
+        Package['openafs-krb5'],
+      ],
+    }
+
+    package { 'dkms-openafs':
+      ensure  => present,
+      require => [
+        Package['kernel-devel'],
+        Package['gcc'],
+        Package['dkms'],
+      ],
+      before  => [
+        Package['openafs-client'],
+        Package['openafs-krb5'],
+      ],
+    }
+
+    file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-OPENAFS':
+      ensure  => present,
+      replace => true,
+      source  => 'puppet:///modules/openafs/RPM-GPG-KEY-OPENAFS',
+      before  => [
+        Package['openafs-client'],
+        Package['openafs-krb5'],
+      ],
+    }
+
+    file { '/etc/yum.repos.d/openafs-rhel.repo':
+      ensure  => present,
+      replace => true,
+      source  => 'puppet:///modules/openafs/openafs-rhel.repo',
+      before  => [
+        Package['openafs-client'],
+        Package['openafs-krb5'],
+      ],
+    }
+
+    file { '/var/cache/openafs':
+      ensure => directory,
+    }
+
+  } else {
+    $openafs_path = '/etc/openafs'
+  }
+
+  file { "$openafs_path/afs.conf.client":
     ensure  => present,
     replace => true,
     source  => 'puppet:///modules/openafs/afs.conf.client',
     require => Package['openafs-client'],
   }
 
-  file { '/etc/openafs/CellServDB':
+  file { "$openafs_path/CellServDB":
     ensure  => present,
     replace => true,
     source  => 'puppet:///modules/openafs/CellServDB',
     require => Package['openafs-client'],
   }
 
-  file { '/etc/openafs/ThisCell':
+  file { "$openafs_path/ThisCell":
     ensure  => present,
     replace => true,
     content => template('openafs/ThisCell.erb'),
     require => Package['openafs-client'],
   }
 
-  file { '/etc/openafs/cacheinfo':
+  file { "$openafs_path/cacheinfo":
     ensure  => present,
     replace => true,
     content => template('openafs/cacheinfo.erb'),
     require => Package['openafs-client'],
   }
 
+  service { 'openafs-client':
+    ensure    => running,
+    require   => [
+      File["$openafs_path/CellServDB"],
+    ],
+  }
 }
