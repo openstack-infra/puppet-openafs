814aaf6b7db685bb7e15530778c218efed9d73d2
Ian Wienand <iwienand@redhat.com>
2018-02-15 14:22:27 +1100

    Add Xenial arm64 support
    Add arm64 support for Xenial hosts, based on the openstack-infra ppa
    with suitable patches ported in.
    
    Change-Id: Iec5921769db7cab15b66f0d82c1705c799dd6ae8

--- manifests/client.pp
+++ manifests/client.pp
@@ -97,9 +97,27 @@ class openafs::client (
   } else {
     $openafs_path = '/etc/openafs'
 
-    $dkms_packages = [
-      'linux-headers-generic',
-    ]
+    if (${::architecture} == 'aarch64') {
+      if (${::libdistcodename} != 'xenial') {
+        fail('arm64 only supported on Xenial at this time')
+      }
+      # Note arm64 == aarch64 ...
+
+      # ARM64 wasn't supported in openafs until
+      #  https://gerrit.openafs.org/11940
+      # This custom PPA has 1.8.x packages with these patches
+      include ::apt
+      apt::ppa { 'ppa:openstack-ci-core/openafs-arm64': }
+
+      # We use the HWE kernel for aarch
+      $dkms_packages = ['linux-generic-hwe-16.04',
+                        'linux-headers-generic-hwe-16.04',]
+      $dkms_requires = [Apt::Ppa['ppa:openstack-ci-core/openafs-arm64'],
+                        Class['apt::update'],]
+    } else {
+      $dkms_packages = ['linux-headers-generic',]
+      $dkms_requires = []
+    }
 
     $openafs_dkms = 'openafs-modules-dkms'
     package { $dkms_packages:
@@ -109,6 +127,7 @@ class openafs::client (
         Package['openafs-krb5'],
         Package[$openafs_dkms],
       ],
+      require => $dkms_requires,
     }
     package { $openafs_dkms:
       ensure => present,
